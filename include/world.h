f5b2911c94f7190ee8dc3f6d35b6684bcf274ee3
CelestialDelta <deltaplaythroughs65@gmail.com>
2017-01-06 20:29:38 -0500

    Implemented state handling for individual entities.

--- include/world.h
+++ include/world.h
@@ -2,6 +2,7 @@
 
 #include <SDL2/SDL.h>
 #include <vector>
+#include <map>
 #include <iostream>
 
 #include <glm/glm.hpp>
@@ -11,13 +12,19 @@
 class Entity;
 class Player;
 
+enum class ENTITY_ID;
+enum class ENTITY_STATE_ID;
+
 class World
 {
 public:
 	World(SDL_Renderer* renderer = nullptr, const glm::vec2& bounds = glm::vec2());
 	~World();
 	
-	void handle(EVENT event, float dt);
+	void change_state(GAMESTATE_ID id);
+	void freeze();
+	
+	void handle(KEY_EVENT event, float dt);
 	void update(float dt);
 	void draw() const;
 	
@@ -25,8 +32,13 @@ public:
 	
 	const glm::vec2& bounds() const;
 	SDL_Renderer* renderer() const;
+	const std::vector<Entity*>& entities() const;
+	
+	bool frozen() const;
 private:
+	bool _frozen;
 	SDL_Renderer* _renderer;
 	std::vector<Entity*> _entities;
+	std::map<GAMESTATE_ID, std::map<ENTITY_ID, ENTITY_STATE_ID>> _statemap;
 	glm::vec2 _bounds;
 };
--- src/world.cpp
+++ src/world.cpp
@@ -2,16 +2,29 @@
 #include "../include/entity.h"
 
 World::World(SDL_Renderer* renderer, const glm::vec2& bounds)
-	: _renderer(renderer), _entities(), _bounds(bounds)
+	: _renderer(renderer), _entities(), _statemap(), _bounds(bounds), _frozen()
 {
 	if(_renderer && bounds != glm::vec2())
 	{
 		Player* player = new Player(this, glm::vec2(), 50.0f, glm::vec2(400, 400), glm::vec2(13, 15), 0, 1.0f, 10.0f);
 		_entities.push_back(player);
 		
-		Asteroid* asteroid = new Asteroid(this, glm::vec2(40, 40), glm::vec2(200, 200), glm::vec2(80, 70), 23);
+		Model model("../res/text.txt");
+		
+		//std::cout << model.vertices().size() << std::endl;
+		
+		Asteroid* asteroid = new Asteroid(this, model, glm::vec2(40, 40), glm::vec2(200, 200), glm::vec2(80, 70), 23);
 		_entities.push_back(asteroid);
 	}
+	
+	std::map<ENTITY_ID, ENTITY_STATE_ID> run_map;
+	run_map[ENTITY_ID::PLAYER] = ENTITY_STATE_ID::PLAYER_DEFAULT;
+	
+	std::map<ENTITY_ID, ENTITY_STATE_ID> deb_map;
+	deb_map[ENTITY_ID::PLAYER] = ENTITY_STATE_ID::PLAYER_RIGID;
+	
+	_statemap[GAMESTATE_ID::RUNNING] = run_map;
+	_statemap[GAMESTATE_ID::DEBUG] = deb_map;
 }
 
 World::~World()
@@ -23,7 +36,26 @@ World::~World()
 	}
 }
 
-void World::handle(EVENT event, float dt)
+void World::change_state(GAMESTATE_ID id)
+{
+	for(size_t i = 0; i < _entities.size(); i++)
+	{
+		Entity* entity = _entities.at(i);
+		ENTITY_STATE_ID state = _statemap[id][entity->id()];
+		if(entity->id() == ENTITY_ID::PLAYER)
+		{
+			//make Entity::push_state virtual function
+			static_cast<Player*>(entity)->push_state(state);
+		}
+	}
+}
+
+void World::freeze()
+{
+	_frozen = !_frozen;
+}
+
+void World::handle(KEY_EVENT event, float dt)
 {
 	for(size_t i = 0; i < _entities.size(); i++)
 	{
@@ -60,4 +92,14 @@ const glm::vec2& World::bounds() const
 SDL_Renderer* World::renderer() const
 {
 	return _renderer;
+}
+
+const std::vector<Entity*>& World::entities() const
+{
+	return _entities;
+}
+
+bool World::frozen() const
+{
+	return _frozen;
 }
